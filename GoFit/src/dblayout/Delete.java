81b245795358af712ae3622237ef0417469cb97e
gtay <gillian.tay.tay@gmail.com>
2014-04-21 16:50:38 -0400

    finished delete
    for now it's all deleting by id but once the objects are generated for
    each type then we can delete by object (i.e.: goal, exercise, workout)

--- GoFit/src/dblayout/Delete.java
+++ GoFit/src/dblayout/Delete.java
@@ -1,15 +1,17 @@
 package dblayout;
 
+import android.content.ContentValues;
+
 public class Delete extends DatabaseConnector {
 	public Delete() { } //empty constructor
 	
-	public void deleteGoal(long id) {
+	public void deleteGoalById(long id) {
 		open(); // open the database
 	    database.delete("goal", "_id=" + id, null);
 	    close(); // close the database
 	}
 	
-	public void deleteWorkout(long id) {
+	public void deleteWorkoutById(long id) {
 		open(); // open the database
 	    database.delete("workout", "_id=" + id, null);
 	    close(); // close the database
@@ -19,10 +21,20 @@ public class Delete extends DatabaseConnector {
 	public void deleteImage(long id) {
 		open();
 		//have to update with an empty string which means need to check if empty string for path (if path not found etc)
+		ContentValues values = new ContentValues();
+		values.put("image_path", "");
+		database.update("workout", values, "_id=" + id, null);
 		close();
 	}
 	
 	//delete audio_file associated with workout
+	public void deleteAudio(long id) {
+		open();
+		ContentValues values = new ContentValues();
+		values.put("audio_file", "");
+		database.update("workout", values, "_id=" + id, null);
+		close();
+	}
 	
 	public void deleteAchievement(long id) {
 		open(); // open the database
