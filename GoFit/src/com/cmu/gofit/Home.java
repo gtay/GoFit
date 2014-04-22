3518d3c9c1fd27d945442bcb5b889e8148a2b362
Lawrence <ltsang@andrew.cmu.edu>
2014-04-21 23:21:12 -0400

    Added navigation support for Home screen

--- GoFit/src/com/cmu/gofit/Home.java
+++ GoFit/src/com/cmu/gofit/Home.java
@@ -1,7 +1,60 @@
 package com.cmu.gofit;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Home extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState); // call superclass's version
+		setContentView(R.layout.gofit_home); // inflate the GUI
+
+		Button exercisesButton = (Button) findViewById(R.id.home_button1);
+		exercisesButton.setOnClickListener(exercisesClicked);		
+		Button workoutsButton = (Button) findViewById(R.id.home_button2);
+		workoutsButton.setOnClickListener(workoutsClicked);		
+		Button goalsButton = (Button) findViewById(R.id.home_button3);
+		goalsButton.setOnClickListener(goalsClicked);		
+		Button achievementsButton = (Button) findViewById(R.id.home_button4);
+		achievementsButton.setOnClickListener(achievementsClicked);
+	}
+	
+	OnClickListener exercisesClicked = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent(Home.this, Exercises.class);
+			startActivity(intent);
+		}
+	};
+	
+	OnClickListener workoutsClicked = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent(Home.this, Workouts.class);
+			startActivity(intent);
+		}
+	};
+	
+	OnClickListener goalsClicked = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent(Home.this, Goals.class);
+			startActivity(intent);
+		}
+	};
+	
+	OnClickListener achievementsClicked = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent(Home.this, Achievements.class);
+			startActivity(intent);
+		}
+	};
 
 }
