cd296bbd4a77eda8b4a2735724c27aba9947abb3
gtay <gillian.tay.tay@gmail.com>
2014-04-28 15:19:18 -0400

    completed insert queries
    everything done but user and achievements don't have insert so we don't
    insert more than one. they are inserted on create, and then updated

--- GoFit/src/dblayout/Create.java
+++ GoFit/src/dblayout/Create.java
@@ -1,5 +1,6 @@
 package dblayout;
 
+import android.content.ContentValues;
 import android.content.Context;
 
 public class Create extends DatabaseConnector {
@@ -8,6 +9,22 @@ public class Create extends DatabaseConnector {
 	public Create(Context context) {
 		// create a new DatabaseOpenHelper
 	    databaseOpenHelper =  new DatabaseOpenHelper(context, DATABASE_NAME, null, 1);
+	    
+	    //initiate user and achievement to be updated with real values
+	    open();
+	    ContentValues user_values = new ContentValues();
+	    user_values.put("name", "name");
+	    user_values.put("age", "age");
+	    user_values.put("height", "height");
+	    user_values.put("weight", "weight");
+	    database.insert(USER_TABLE, null, user_values);
+	    
+	    ContentValues achieve_values = new ContentValues();
+	    achieve_values.put("num_completed", "0");
+	    achieve_values.put("fastest_time", "None");
+	    database.insert(USER_TABLE, null, achieve_values);
+	    
+	    close();
 	} // end Create
 
 }
--- GoFit/src/dblayout/Insert.java
+++ GoFit/src/dblayout/Insert.java
@@ -1,5 +1,56 @@
 package dblayout;
 
-public class Insert {
+import android.content.ContentValues;
+import android.util.Log;
+import entities.*;
 
+public class Insert extends DatabaseConnector {
+
+	public Insert() {}
+	
+	//no adds for user and achievement since only one row is expected
+	//any changes are done with update
+	
+	public void addGoal(Goal goal) {
+		//for debugging
+		Log.d("addGoal", goal.toString());
+		open();
+		
+		ContentValues values = new ContentValues();
+		values.put("name", goal.getName());
+		values.put("end_date", goal.getEndDate());
+		values.put("progress", goal.getProgress());
+		
+		database.insert(GOAL_TABLE, null, values);
+		close();
+	}
+	
+	public void addWorkout(Workout workout) {
+		//for debugging
+		Log.d("addWorkout", workout.toString());
+		open();
+		
+		ContentValues values = new ContentValues();
+		values.put("goal_id", workout.getGoalId());
+		values.put("date", workout.getDate());
+		values.put("details", workout.getDetails());
+		values.put("image_path", workout.getImagePath());
+		values.put("audio_file", workout.getAudioPath());
+		
+		database.insert(WORK_TABLE, null, values);
+		close();
+	}
+	
+	public void addExercise(Exercise exercise) {
+		//for debugging
+		Log.d("addExercise", exercise.toString());
+		open();
+		
+		ContentValues values = new ContentValues();
+		values.put("name", exercise.getName());
+		values.put("details", exercise.getDetails());
+		
+		database.insert(EXERCISE_TABLE, null, values);
+		close();
+	}
 }
