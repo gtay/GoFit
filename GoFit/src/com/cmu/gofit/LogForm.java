e02d95d90fd4c460b2798b03734a288d606901d8
Lawrence <ltsang@andrew.cmu.edu>
2014-05-01 23:11:07 -0400

    Added audio support for LogForm

--- GoFit/src/com/cmu/gofit/LogForm.java
+++ GoFit/src/com/cmu/gofit/LogForm.java
@@ -1,13 +1,17 @@
 package com.cmu.gofit;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -26,6 +30,15 @@ public class LogForm extends Activity {
 	private Uri photoUri;
 	private String todayString;
 	private static String imagePath;
+	private static String timeStamp;
+	
+    private static final String LOG_TAG = "AudioRecordTest";
+    private static String mFileName = null;
+    
+    private Button addAudioButton;
+    private boolean mStartRecording = true;
+    private MediaRecorder mRecorder = null;
+
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) 
@@ -35,7 +48,8 @@ public class LogForm extends Activity {
 		
 		Button addPhotoButton = (Button) findViewById(R.id.lf_button1);
 		addPhotoButton.setOnClickListener(addPhotoClicked);
-		
+		addAudioButton = (Button) findViewById(R.id.lf_button2);
+		addAudioButton.setOnClickListener(addAudioClicked);
 		// get today's date and convert to string
 		DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
 		Date today = Calendar.getInstance().getTime();
@@ -52,6 +66,20 @@ public class LogForm extends Activity {
 			photoUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
 			camera.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
 			startActivityForResult(camera, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+		}
+	};
+	
+	OnClickListener addAudioClicked = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			onRecord(mStartRecording);
+			if (mStartRecording) {
+				addAudioButton.setText("Stop recording");
+			} else {
+				addAudioButton.setText("Done recording!");
+				addAudioButton.setEnabled(false);
+			}
+			mStartRecording = !mStartRecording;
 		}
 	};
 	
@@ -84,13 +112,13 @@ public class LogForm extends Activity {
 	    // Create the storage directory if it does not exist
 	    if (! mediaStorageDir.exists()){
 	        if (! mediaStorageDir.mkdirs()){
-	            Log.d("MyCameraApp", "failed to create directory");
+	            Log.d("GoFit", "failed to create directory");
 	            return null;
 	        }
 	    }
 
 	    // Create a media file name
-	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+	    timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
 	    File mediaFile;
 	    if (type == MEDIA_TYPE_IMAGE){
 	    	imagePath = mediaStorageDir.getPath() + File.separator +
@@ -102,4 +130,51 @@ public class LogForm extends Activity {
 
 	    return mediaFile;
 	}
+	
+    private void onRecord(boolean start) {
+        if (start) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+    }
+
+    private void startRecording() {
+	    File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+	              Environment.DIRECTORY_MUSIC), "GoFit");
+
+	    // Create the storage directory if it does not exist
+	    if (! mediaStorageDir.exists()){
+	        if (! mediaStorageDir.mkdirs()){
+	            Log.d("GoFit", "failed to create directory");
+	            return;
+	        }
+	    }
+    	
+	    timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+    	mFileName = Environment.getExternalStorageDirectory().getAbsolutePath()
+    			+ File.separator + "Music" + File.separator +
+    			"GoFit" + File.separator + timeStamp + ".3gp";
+    	Log.d("User", mFileName);
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+
 }
